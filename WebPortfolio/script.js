fd7c597ce4113f833b813e956a428a13accd6caa
Mumti Hany Farisa <mumti.hany18@mhs.uinjkt.ac.id>
2021-06-24 17:58:37 +0700

    first commit

---
+++ WebPortfolio/script.js
@@ -0,0 +1,5 @@
+let btnOpen = document.querySelector('p#download');
+
+btnOpen.addEventListener('click', () => {
+    window.open("https://docs.google.com/document/d/1Cw4mKdlYqH0hLW1d4ah60X7idfQiv9Jn/edit", '_blank');
+});
